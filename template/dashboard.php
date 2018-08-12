b137c1c865c191aec3eb64712a3b087c216e748d
Roller Davic <rollerdavic@gmail.com>
2018-08-13 00:22:03 +0700

    Update

--- template/dashboard.php
+++ template/dashboard.php
@@ -34,44 +34,13 @@
 	</div>
 </div>
 <br>
-<div class="chart border" id="main_chart"></div>
-		<br>
 <div class="row d-flex flex-row">
-	<div class="col-lg-3 col-sm-3">
-		<div class="task border">
-			<div class="sell-task"></div>
-			<div class="price-task text-center bg-light">7500 <span>7500 $</span></div>
-			<div class="buy-task"></div>
-		</div>
-	</div>
-	<div class="col-lg-6 col-sm-6">
-		
-		<div class="controller">
-			<nav>
-			  <div class="nav nav-tabs" id="nav-tab" role="tablist">
-			  	
-			  	<a class="nav-item nav-link active" id="nav-home-tab" data-toggle="tab" href="#nav-home" role="tab" aria-controls="nav-home" aria-selected="true">Limit</a>
-			    <a class="nav-item nav-link" id="nav-home-tab" data-toggle="tab" href="#nav-home" role="tab" aria-controls="nav-home" aria-selected="true">Market</a>
-			    <a class="nav-item nav-link" id="nav-profile-tab" data-toggle="tab" href="#nav-profile" role="tab" aria-controls="nav-profile" aria-selected="false">Stop limit</a>
-			    
-			  </div>
-			</nav>
-			<div class="tab-content border border-top-0" id="nav-tabContent">
-			  	
-			  <div class="tab-pane fade show active" id="nav-home" role="tabpanel" aria-labelledby="nav-home-tab">
-			  		<?php include __DIR__."/forms/limit.php";?>
-			  </div>
-			</div>
-		</div>
+	<div class="col-lg-9 col-sm-3">
 
-		<br>
-		<div class="border" style="padding:30px; min-height: 350px;">
-			<h5>Depth</h5>
-		</div>
-		
+		<div class="chart border" id="main_chart"></div>
 	</div>
-	<div class="col-lg-3">
-		<div id="coinValible">
+	<div class="col-lg-3 col-sm-3">
+		<div id="coinValible" class="border">
 			<nav>
 			  <div class="nav nav-tabs" id="nav-tab" role="tablist">
 			  	
@@ -155,6 +124,55 @@
 			  <div class="tab-pane fade" id="nav-contact" role="tabpanel" aria-labelledby="nav-contact-tab">...</div>
 			</div>
 		</div>
+	</div>
+</div>
+		<br>
+<div class="row d-flex flex-row">
+	<div class="col-lg-3 col-sm-3">
+		<div class="task border">
+			<div class="sell-task"></div>
+			<div class="price-task text-center bg-light">7500 <span>7500 $</span></div>
+			<div class="buy-task"></div>
+		</div>
+	</div>
+	<div class="col-lg-6 col-sm-6">
+		
+		<div class="controller">
+			<nav>
+			  <div class="nav nav-tabs" id="nav-tab" role="tablist">
+			  	
+			  	<a class="nav-item nav-link active" id="nav-home-tab" data-toggle="tab" href="#nav-home" role="tab" aria-controls="nav-home" aria-selected="true">Limit</a>
+			    <a class="nav-item nav-link" id="nav-home-tab" data-toggle="tab" href="#nav-home" role="tab" aria-controls="nav-home" aria-selected="true">Market</a>
+			    <a class="nav-item nav-link" id="nav-profile-tab" data-toggle="tab" href="#nav-profile" role="tab" aria-controls="nav-profile" aria-selected="false">Stop limit</a>
+			    
+			  </div>
+			</nav>
+			<div class="tab-content border border-top-0" id="nav-tabContent">
+			  	
+			  <div class="tab-pane fade show active" id="nav-home" role="tabpanel" aria-labelledby="nav-home-tab">
+			  		<?php include __DIR__."/forms/limit.php";?>
+			  </div>
+			</div>
+		</div>
+
+		<br>
+		<div class="border" style="padding:30px; min-height: 350px;">
+			<h5>Depth</h5>
+		</div>
+		
+	</div>
+	<div class="col-lg-3">
+		<div id="trol">
+			
+			<div class="trade-history border align-items-end"></div>
+
+			<div class="input-group mb-3 input-group-sm" style="margin-top: 5px;">
+			  <input type="text" class="form-control" placeholder="Recipient's username" aria-label="Recipient's username" aria-describedby="button-addon2">
+			  <div class="input-group-append">
+			    <button class="btn btn-outline-secondary" type="button" id="button-addon2">Send</button>
+			  </div>
+			</div>
+		</div>
 		<!--// Trade History -->
 		<div id="history">
 			<h5>Trade History</h5>
@@ -231,9 +249,9 @@
 <script type="text/javascript">
 	$(document).ready(function(){
 		var history = $("#history").height();
-		var parenHeight = $("#coinValible").parent().height();
+		var parenHeight = $(".controller").parent().height();
 		var priceTask = $(".price-task").outerHeight();
-		$("#coinValible").height(parenHeight - history);
+		//$("#coinValible").height($("#main_chart").height());
 		//$(".task").height(parenHeight);
 		$(".sell-task, .buy-task").height((parenHeight - priceTask - 2)/2);
 	});
