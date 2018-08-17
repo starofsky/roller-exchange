ed07fbea509b68d51c11e5f8e8f53664bf76445c
Roller Davic <rollerdavic@gmail.com>
2018-08-18 00:27:47 +0700

    Update

---
+++ template/default/account/menu.php
@@ -0,0 +1,10 @@
+<ul class="list-group">
+  <li class="list-group-item active">Profile</li>
+  <li class="list-group-item"><i class="ti-wallet"></i> Wallet</li>
+  <li class="list-group-item">F2A Authentication</li>
+  <li class="list-group-item">Referrals</li>
+  <li class="list-group-item">Change Password</li>
+  <li class="list-group-item">Login History</li>
+  <li class="list-group-item">Api Authour</li>
+  <li class="list-group-item">Notifications</li>
+</ul>
--- template/default/account/myaccount.php
+++ template/default/account/myaccount.php
@@ -1,34 +1,44 @@
-<h5>All Coins</h5>
-<table class="table">
-	<thead>
-		
-		
-		<th colspan="2">Symbol</th>
-		<th>Wallet</th>
-		<th>Deposit</th>
-		<th>Deposit</th>
-		<th>Withdraw</th>
-		<th>Main Account</th>
-		<th>Transfer</th>
-		<th>Trading Account</th>
-		<th>On Orders</th>
+<h2>Account Manager</h2>
+<div class="row">
+	<div class="col-lg-2">
+		<?php include __DIR__."/menu.php"; ?>
+	</div>
+	<div class="col-lg-10">
+		<div class="card card-body">
+		<h5>All Coins</h5>
+		<table class="table">
+			<thead>
+				
+				
+				<th colspan="2">Symbol</th>
+				<th>Wallet</th>
+				<th>Deposit</th>
+				
+				<th>Withdraw</th>
+				<th>Main Account</th>
+				<th>Transfer</th>
+				<th>Trading Account</th>
+				<th>On Orders</th>
 
-	</thead>
-	<tbody>
-		<?php foreach ($data as $key => $value) { ?>
-			
-		<tr>
-			
-			
-			<td><?php echo $value->name;?></td>
-			<td><?php echo $value->symbol;?></td>
-			<td>Last Prices</td>
-			<td>24 High</td>
-			<td>24 Low</td>
-			<td>24 Volume</td>
-			<td>Fee</td>
-			<td>Status</td>
-		</tr>
-		<?php } ?>
-	</tbody>
-</table>
+			</thead>
+			<tbody>
+				<?php foreach ($data as $key => $value) { ?>
+					
+				<tr>
+					
+					
+					<td><?php echo $value->name;?></td>
+					<td><?php echo $value->symbol;?></td>
+					<td>Last Prices</td>
+					<td><i class="ti-plus"></i></td>
+					<td><i class="ti-plus"></i></td>
+					<td>24 Volume</td>
+					<td>Fee</td>
+					<td>Status</td>
+				</tr>
+				<?php } ?>
+			</tbody>
+		</table>
+		</div>
+	</div>
+</div>
